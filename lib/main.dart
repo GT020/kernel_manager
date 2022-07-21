9e63353522e240b1cb07f43d7ca68fdcd3a070e6
GT020 <gauravtanwar0020@gmail.com>
2022-07-21 08:28:50 +0530

    utils:root: add basic root process which starts at app startup

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,13 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'package:penguin_kernel_manager/app/app.dart';
 import 'package:penguin_kernel_manager/app/core/local_storage/app_storage.dart';
+import 'package:penguin_kernel_manager/app/utils/root_utils.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  //initialize root shell
+  await RootShell.initializeRootShell();
 
   // for initializing local storage
   final appStorage = AppStorage();
