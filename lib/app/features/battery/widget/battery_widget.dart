decd3fdeccc39358f434f04bb698896a0d4e826a
GT020 <gauravtanwar0020@gmail.com>
2022-07-24 15:16:39 +0530

    feature:battery: add charging status

--- lib/app/features/battery/widget/battery_widget.dart
+++ lib/app/features/battery/widget/battery_widget.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:penguin_kernel_manager/app/features/battery/model/battery_model.dart';
 import 'package:penguin_kernel_manager/app/features/battery/widget/battery_current_now_widget.dart';
 import 'package:penguin_kernel_manager/app/features/battery/widget/battery_percentage_widget.dart';
 import 'package:penguin_kernel_manager/app/features/battery/widget/battery_voltage_now_widget.dart';
+import 'package:penguin_kernel_manager/app/utils/read_utils.dart';
+
+final chargingStatusProvider =
+    StreamProvider.family<String, String>((final ref, final String node) {
+  return ReadUtil.readStream(node);
+});
 
 class BatteryWidget extends StatelessWidget {
   final BatteryModel battery;
@@ -15,29 +22,50 @@ class BatteryWidget extends StatelessWidget {
 
     return Wrap(
       children: [
-        Row(
-          children: [
-            Card(
-              child: BatteryPercentageWidget(
-                height: 150,
-                width: width / 2,
-                batteryPercentageNode: battery.percentageNode,
+        Center(
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Consumer(
+                builder: (final context, final ref, final widget) {
+                  final asyncChargingStatus =
+                      ref.watch(chargingStatusProvider(battery.statusNode));
+                  final chargingStatus = asyncChargingStatus.when(
+                    data: (final data) => data,
+                    error: (final e, final s) => '',
+                    loading: () => '',
+                  );
+
+                  return Text(
+                    'Status : $chargingStatus',
+                  );
+                },
               ),
             ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Battery Technology${battery.technology}'),
-                    Text('Battery Health : ${battery.health}'),
-                    Text('Battery TotalCapacity : ${battery.totalCapacity}'),
-                  ],
-                ),
+          ),
+        ),
+        Card(
+          child: BatteryPercentageWidget(
+            height: 150,
+            width: width / 2,
+            batteryPercentageNode: battery.percentageNode,
+          ),
+        ),
+        SizedBox(
+          height: 158,
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Battery Technology${battery.technology}'),
+                  Text('Battery Health : ${battery.health}'),
+                  Text('Battery TotalCapacity : ${battery.totalCapacity}'),
+                ],
               ),
             ),
-          ],
+          ),
         ),
         Card(
           child: Padding(
