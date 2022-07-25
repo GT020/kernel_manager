5399cdc3493698315e31c35034749a5a1cbdc64e
GT020 <gauravtanwar0020@gmail.com>
2022-07-25 15:38:48 +0530

    feature:ram: need to rethink the overall ram structure

--- lib/app/features/ram/widget/ram_widget.dart
+++ lib/app/features/ram/widget/ram_widget.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:penguin_kernel_manager/app/features/ram/model/ram_model.dart';
+import 'package:penguin_kernel_manager/app/utils/read_utils.dart';
+import 'package:penguin_kernel_manager/app/utils/root_utils.dart';
+
+final ramStreamProvider = StreamProvider.family<String, String>(
+  (final Ref ref, final node) {
+    return ReadUtil.catStream(node);
+  },
+);
 
 class RamWidget extends StatelessWidget {
   final RamModel ram;
@@ -7,6 +16,22 @@ class RamWidget extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
-    return const SizedBox();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Consumer(
+        builder: (final context, final ref, final child) {
+          final AsyncValue<String> asyncRam =
+              ref.watch(ramStreamProvider(ram.usedRamNode));
+
+          return ListView(
+            children: [
+              ...asyncRam.value!.split('\n').map((final line) {
+                return Card(child: Text(line));
+              }).toList(),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
